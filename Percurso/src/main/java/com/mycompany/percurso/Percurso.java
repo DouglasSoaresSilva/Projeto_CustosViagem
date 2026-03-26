18317bf4e5d86d33b34d4ca95a115fd98ab2968c
Douglas Soares <douglas201003@gmail.com>
2026-03-26 13:36:59 -0300

    Add files via upload

---
+++ Percurso/src/main/java/com/mycompany/percurso/Percurso.java
@@ -0,0 +1,22 @@
+package com.mycompany.percurso;
+/**
+ *
+ * @author Douglas Soares Silva
+ */
+public class Percurso {
+        private double kmPercorrido;
+        private double valorCombustivel;
+        private double valorPedagio;
+        
+        public void cadastrarPercurso(double kmPercorrido, double valorCombustivel, double valorPedagio){
+            this.kmPercorrido = kmPercorrido;
+            this.valorCombustivel = valorCombustivel;
+            this.valorPedagio = valorPedagio;
+        }
+        
+        public String listarPercurso(){
+        return "Km percorrido: " + kmPercorrido +
+                "\nValor do combustível: " + valorCombustivel + 
+                "\nValor do pedágio: " + valorPedagio;
+ }
+}
