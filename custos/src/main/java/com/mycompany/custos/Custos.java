ef27eadf9dbdcb12a2373fe8243e6095e878df6b
Caio Novaes <caionovaes2207@gmail.com>
2026-04-01 13:37:30 -0300

    Add files via upload

---
+++ custos/src/main/java/com/mycompany/custos/Custos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.custos;
+
+/**
+ *
+ * @author Admin
+ */
+public class Custos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
